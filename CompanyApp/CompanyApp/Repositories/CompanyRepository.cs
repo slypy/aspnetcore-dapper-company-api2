e7360427283bd0d5d9b4408603a88ddcc049a13f
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2022-09-05 23:26:53 +0800

    Implement CreateCompany method

--- CompanyApp/CompanyApp/Repositories/CompanyRepository.cs
+++ CompanyApp/CompanyApp/Repositories/CompanyRepository.cs
@@ -36,5 +36,19 @@ namespace CompanyApp.Repositories
                 return companies;
             }
         }
+
+        public async Task<Company> CreateCompany(Company company)
+        {
+            var query = "INSERT INTO Companies (Name, Address, Country) " +
+                        "VALUES (@Name, @Address, @Country); " +
+                        "SELECT * FROM Companies WHERE Id = SCOPE_IDENTITY()";
+
+            using (var connection = _context.CreateConnection())
+            {
+                var newCompany = await connection.QuerySingleOrDefaultAsync<Company>(query, company);
+                
+                return newCompany;
+            }
+        }
     }
 }

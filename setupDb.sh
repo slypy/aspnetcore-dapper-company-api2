5a2875a8fd0c13fd56e7ecb3b9309f02583972ee
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2022-09-05 22:44:40 +0800

    Update setupDb.sh to executable file

--- setupDb.sh
+++ setupDb.sh
@@ -4,7 +4,7 @@
 #     'ls -1' sorts the file names based on the current locale 
 #     and presents them in a single column
 for i in *.sql; do 
-    sqlcmd -S 192.168.1.8,4066 -U SA -P ***** -i $i
+    sqlcmd -S localhost,1433 -U SA -P S-123123s -i $i
 done
 
 # TODO: Test for find command if applicable
